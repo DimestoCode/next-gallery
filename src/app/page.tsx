1ebea3452217bb609ccb735ca858ebd07281f4f2
dmytroandoniev <dmytro.andoniev@walkme.com>
2024-04-28 17:44:54 +0300

    Initialized database skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const urls = [
   "https://utfs.io/f/034b289a-79d6-41ff-a576-6984e8c5e95b-1d.jpeg",
   "https://utfs.io/f/c13500bd-1f27-48e3-88ad-897d3eb8c669-1f.avif",
@@ -9,10 +11,19 @@ const mockImages = urls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="" />
